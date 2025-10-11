396ffe5027d17ac33ae02633306d1fd4d6d00c26
David Michaels <dmichaels@gmail.com>
2025-10-11 12:15:37 -0400

    etc

--- jeopardy.py
+++ jeopardy.py
@@ -1,5 +1,7 @@
+import gzip
 import json
 import random
+import shutil
 import re
 import sys
 
@@ -7,8 +9,11 @@ import sys
 # https://www.kaggle.com/datasets/aravindram11/jeopardy-dataset-updated
 #
 jeopardy_file = "jeopardy_questions_19840910_20120127.json"
+jeopardy_zip_file = "jeopardy_questions_19840910_20120127.json.gz"
 
 def main():
+
+    unzip(jeopardy_zip_file, jeopardy_file)
 
     with open(jeopardy_file, "r") as f:
         data = json.load(f)
@@ -34,6 +39,11 @@ def display(data: list[dict]):
 def normalize(s: str) -> str:
     s = s.strip()
     return re.sub(r"\s+", " ", s.replace("<br />", " ").strip().strip("\"").strip("'").replace("\\'", "'").strip())
+
+def unzip(jeopardy_zip_file: str, jeopardy_file: str) -> str:
+    with gzip.open(jeopardy_zip_file, "rb") as fin:
+        with open(jeopardy_file, "wb") as fout:
+            shutil.copyfileobj(fin, fout)
 
 if __name__ == "__main__":
     main()
